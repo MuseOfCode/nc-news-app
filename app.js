2c241ade3bcda6a4076641a792b7b51e679f8a35
Ken'Terria Reaves <kenterria.z.reaves@gmail.com>
2025-03-10 12:28:17 +0000

    feat: implement GET /api/topics endpoint to pass tests

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const { getEndpoints } = require("./controllers/api.controller");
+const { getTopics } = require("./controllers/topics.controller");
 
 app.get("/api", getEndpoints);
+
+app.get("/api/topics", getTopics);
 
 module.exports = app;
---
+++ controllers/topics.controller.js
@@ -0,0 +1,8 @@
+require("../app");
+const { fetchTopics } = require("../models/topics.model");
+
+exports.getTopics = (req, res) => {
+  fetchTopics().then((topics) => {
+    res.status(200).send({ topics: topics });
+  });
+};
